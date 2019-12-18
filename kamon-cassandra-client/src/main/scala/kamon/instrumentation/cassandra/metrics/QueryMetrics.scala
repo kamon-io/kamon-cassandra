3d537cfceaf349641feea747a69a0716dc3ba340
Mladen Subotic <mladens42@gmail.com>
2019-12-18 09:55:13 +0100

    Formatting

--- kamon-cassandra-client/src/main/scala/kamon/instrumentation/cassandra/metrics/QueryMetrics.scala
+++ kamon-cassandra-client/src/main/scala/kamon/instrumentation/cassandra/metrics/QueryMetrics.scala
@@ -8,11 +8,26 @@ import kamon.metric.{Counter, InstrumentGroup}
 import kamon.trace.Span
 
 object QueryMetrics {
-  val Errors                = Kamon.counter(name = "cassandra.query.errors", description = "Count of executions that resulted in error")
-  val Timeouts              = Kamon.counter(name = "cassandra.query.timeouts", description =  "Count of executions that timed-out")
-  val RetriedExecutions     = Kamon.counter(name = "cassandra.query.retries", description = "Count of executions that were retries")
-  val SpeculativeExecutions = Kamon.counter(name = "cassandra.query.speculative", description = "Count of executions that were triggered by speculative execution strategy")
-  val CanceledExecutions    = Kamon.counter(name = "cassandra.query.cancelled", description = "Count of executions that were cancelled mid-flight")
+  val Errors = Kamon.counter(
+    name = "cassandra.query.errors",
+    description = "Count of executions that resulted in error"
+  )
+  val Timeouts = Kamon.counter(
+    name = "cassandra.query.timeouts",
+    description = "Count of executions that timed-out"
+  )
+  val RetriedExecutions = Kamon.counter(
+    name = "cassandra.query.retries",
+    description = "Count of executions that were retries"
+  )
+  val SpeculativeExecutions = Kamon.counter(
+    name = "cassandra.query.speculative",
+    description = "Count of executions that were triggered by speculative execution strategy"
+  )
+  val CanceledExecutions = Kamon.counter(
+    name = "cassandra.query.cancelled",
+    description = "Count of executions that were cancelled mid-flight"
+  )
 
   def instrumentsForHost(host: Host, clusterName: String): QueryInstruments = new QueryInstruments(CassandraInstrumentation.targetFromHost(host, clusterName))
 
@@ -22,15 +37,16 @@ object QueryMetrics {
       CassandraInstrumentation.tagSpanWithTarget(targetNode, span)
     }
 
-    val errors: Counter   = register(Errors)
+    val errors: Counter = register(Errors)
     val timeouts: Counter = register(Timeouts)
 
     /*Here it would be more valuable to tag with host that's being retried or speculated on than
     * one defined by a policy so we are dropping it altogether */
-    val retries: Counter                = RetriedExecutions.withoutTags()
-    val speculative: Counter            = SpeculativeExecutions.withoutTags()
-    val cancelled: Counter              = CanceledExecutions.withoutTags()
+    val retries: Counter = RetriedExecutions.withoutTags()
+    val speculative: Counter = SpeculativeExecutions.withoutTags()
+    val cancelled: Counter = CanceledExecutions.withoutTags()
   }
+
 }
 
 
